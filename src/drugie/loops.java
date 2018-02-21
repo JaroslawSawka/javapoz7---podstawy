0e3f47814b7798ccfa6096627b2e74cecdf7facc
JaroslawSawka <sawkajaroslaw@gmail.com>
2018-02-21 20:32:56 +0100

    loops

--- src/drugie/loops.java
+++ src/drugie/loops.java
@@ -11,14 +11,51 @@ public class loops {
     private static void forLoopExample() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Insert number of elements to multiply:");
+        System.out.println("Insert number of elements:");
         int n = scanner.nextInt();
 
+        System.out.println();
+        System.out.println();
+
         for (int i = 0; i <n; i++){
-            System.out.println("Insert number:");
-            int number = scanner.nextInt();
-            System.out.println((i+1) + " Pow(" + number +") = " + (number * number));
+            System.out.println(2*(i+1));
         }
+
+        System.out.println();
+        System.out.println();
+
+        for (int i = 0; i <n; i++){
+            if (i%2 == 0){
+                System.out.println(i);
+            }
+        }
+
+        System.out.println();
+        System.out.println();
+
+        for (int i = 0; i <n; i++){
+            if (i%3 == 0){
+                System.out.println(i);
+            }
+        }
+
+        System.out.println();
+        System.out.println();
+
+        for (int i = 0; i <n; i++){
+            System.out.println("a");
+        }
+
+        System.out.println();
+        System.out.println();
+
+        int sum = 0;
+        for (int i = 1; i <=n; i++){
+            sum +=i;
+        }
+
+        System.out.println(sum);
+
 
         System.out.println("End of program.");
     }
